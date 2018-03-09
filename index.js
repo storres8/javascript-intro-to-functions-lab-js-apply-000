5e94b7f9f0e307e46ab0d5f573c1eaf9eea675ba
Steven Torres <stevetorres7562@gmail.com>
2018-03-09 23:20:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,16 +18,10 @@ function sayHiToGrandma(string){
 if (string.toLowerCase() === string) {
   return "I can't hear you!";
   } 
-} 
-
-function sayHiToGrandma(string) {
-  if (string.toUpperCase()===string) {
+if (string.toUpperCase()===string) {
     return "YES INDEED!";
   }
-}
-
-function sayHiToGrandma(string) {
-  if(string.toLowerCase()===string){
-    return "I can't hear you!";
+if(string === "I love you, Grandma"){
+    return "I love you, too.";
   }
 }
