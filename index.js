4c0d78bc2a2defa77990e2bda4f3cf81ff1b0624
Steven Torres <stevetorres7562@gmail.com>
2018-03-09 23:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (string.toLowerCase() === string) {
 if (string.toUpperCase()===string) {
     return "YES INDEED!";
   }
-if(string === "I love you, Grandma"){
+if(string === "I love you, Grandma."){
     return "I love you, too.";
   }
 }
